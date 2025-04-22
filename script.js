0143d2f3bb2af2a9c92d9ea971367fe2d84b5d7b
kira07sj <kirubeldagne1@gmail.com>
2025-04-22 18:10:29 +0300

    add animation & functions for shipment details

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("is working")
+document.getElementById("Recipient").addEventListener("click", function() {
+    document.getElementsByClassName("shipment-details")[0].style.display = "none";
+    document.getElementsByClassName("shipment-details-2")[0].style.display = "flex";
+    
+});
+document.getElementById("sender").addEventListener("click", function() {
+    document.getElementsByClassName("shipment-details")[0].style.display = "flex";
+    document.getElementsByClassName("shipment-details-2")[0].style.display = "none";
+    
+});
+
